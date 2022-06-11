3579949e880414c37438a9e1396084b7bbed90cf
bulhwi <mm225012@gmail.com>
2022-06-11 21:28:22 +0900

    sample UI

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import Box from './components/Box';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './components/Counter';
+// import Box from './components/Box';
 
 const App = () => {
+  // const [visible, setVisible] = useState(true);
+  // const onPress = () => {
+  //   setVisible(!visible);
+  // };
+
   return (
-    <SafeAreaView>
-      <Box rounded={true} size="large" color="blue" />
+    <SafeAreaView style={styles.full}>
+      <Counter />
+      {/*<Button title="토글" onPress={onPress} />*/}
+      {/*{visible && <Box rounded={true} size="large" color="blue" />}*/}
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+    backgroundColor: 'cyan',
+  },
+});
 
 export default App;
